4fb490d76bc3a25a30b8276833aa58934461afc2
ShrijanK <johnta75@gmail.com>
2016-09-20 11:47:14 +0545

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-mohit = "Randi ko choro bhalu muji"
-sam = raw_input("Is mohit void ? : [Y/N]").lower()
-if(sam = "y"):
-{
-	print("Of course that faggot has nothing downstaris")
-}
-else:
-	print("So you are telling me he has something downstairs? Hell nahh nigga")
